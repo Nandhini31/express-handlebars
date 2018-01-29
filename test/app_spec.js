f403af6b994be66c27f8249cc40fd13a7dd64366
Nandhini Narasimhan <nandhini.narasimhan@ladbrokescoral.com>
2018-01-29 14:33:13 +0000

    initial tests passing

---
+++ test/app_spec.js
@@ -0,0 +1,33 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var app = require('../app');
+
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('App', function() {
+
+  describe('check application is reachable', function() {
+    it('responds with status 200', function(done) {
+      chai.request('http://localhost:3000')
+        .get('/')
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe('check application is reachable', function() {
+    it('responds with status 400 path not found', function(done) {
+      chai.request('http://localhost:3000')
+        .get('/values')
+        .end(function(err, res) {
+          expect(res).to.have.status(404);
+          done();
+        });
+    });
+  });
+
+});
